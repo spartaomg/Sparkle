7b90cdf41ffcbdeaeec2fd2aa0be7e46de822ad4
spartaomg <spartaofomg@gmail.com>
2020-01-04 11:36:32 -0500

    010420 - "include file list" feature

--- Sparkle C64 Demo Tool/My Project/AssemblyInfo.vb
+++ Sparkle C64 Demo Tool/My Project/AssemblyInfo.vb
@@ -12,7 +12,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("Sparkle C64 Demo Tool")>
 <Assembly: AssemblyCompany("OMG")>
 <Assembly: AssemblyProduct("Sparkle C64 Demo Tool")>
-<Assembly: AssemblyCopyright("Copyright © Sparta/OMG 2019")>
+<Assembly: AssemblyCopyright("Copyright © Sparta/OMG 2019-2020")>
 <Assembly: AssemblyTrademark("")>
 
 <Assembly: ComVisible(False)>
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.2.0.0")>
-<Assembly: AssemblyFileVersion("1.2.0.0")>
+<Assembly: AssemblyVersion("1.3.0.0")>
+<Assembly: AssemblyFileVersion("1.3.0.0")>
